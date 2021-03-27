db8a4b8fa4933b653370049b15afa915419310fa
semgoSE <semgoSE@gmail.com>
2021-03-28 02:32:52 +0300

    dd

--- src/server_api.js
+++ src/server_api.js
@@ -1,12 +1,13 @@
 import axios from "axios";
-const URL = "https://195.161.62.85:3000/";
+const URL = "https://api-waste.hhos.ru/mth/";
 
 export function send(method, obj) {
-    const a = new Promise((resolve, reject) => {
-        axios.post(URL + method, obj).then((e) => {
-            console.log(e)
+    return new Promise((resolve, reject) => {
+        axios.post(URL, JSON.stringify({ type: method, obj })).then((e) => {
             resolve(e.data);
-        }).catch((e) => console.log(e))
-    });
-    return a;
+        })
+        .catch((e) => {
+            reject(e)
+        })
+    })
 }
