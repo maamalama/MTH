66b044cc2ab628017fab27778754488dee15d26c
semgoSE <semgoSE@gmail.com>
2021-03-28 13:50:52 +0300

    lasr

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
-import { Alert, AdaptivityProvider, AppRoot, ConfigProvider, PanelHeader, Root, View, Panel, FormLayout, FormLayoutGroup, FormItem, Input, Button, Spinner, CustomSelect, CardGrid, Card, Group, Cell, Radio, ModalPage, ModalRoot, ModalPageHeader, ScreenSpinner, Header, SimpleCell, Div, Title } from '@vkontakte/vkui';
+import { Alert, AdaptivityProvider, AppRoot, ConfigProvider, PanelHeader, Root, View, Panel, FormLayout, FormLayoutGroup, FormItem, Input, Button, Spinner, CustomSelect, CardGrid, Card, Group, Cell, Radio, ModalPage, ModalRoot, ModalPageHeader, ScreenSpinner, Header, SimpleCell, Div, Title, Textarea } from '@vkontakte/vkui';
 import { io } from "socket.io-client"
 import '@vkontakte/vkui/dist/vkui.css';
 import './css/style.css';
 import vkQr from '@vkontakte/vk-qr';
 import { send } from './server_api';
-import "./vivify.css";
+import { insideBoundingBox, getBoundingBox } from 'geolocation-utils';
+import './style.css'
 
+const locations1 = [
+	{ lat:55.7172559, lon: 37.562876 },
+  ]
+
+  const locations2 = [
+	{ lat:55.7175200, lon: 37.563694 },
+  ]
 
 class App extends Component {
 	state = {
@@ -16,7 +24,7 @@ class App extends Component {
 		activePanelAdmin: "create_user",
 		activePanelUser: "main",
 		activeView: "user",
-		user_id: null,
+		user_id: "",
 		user: {
 			name: null,
 			sex: null,
@@ -33,47 +41,40 @@ class App extends Component {
 
 
 	componentDidMount() {
+		const socket = new io("wss://81.177.165.209:3000", { transports: ["websocket"]})
+		socket.onAny((d) => console.log(d))
 		if (window.location.hash === "#admin") {
 			this.setState({ activeView: "admin", activePanelAdmin: "create_user" })
 
 		} else {
-			this.setState({ user_id: window.location.hash.split("user")[1] })
+			this.setState({ user_id: window.location.hash.split("user")[1], socket })
 			this.getGeolcation(); 
 			send("quest", {}).then(data => {
-				this.setState({ dataQuestions: [data.question] })
+				this.setState({ dataQuestions: data.question })
 			})
-			
 		}
 
 
 	}
 
 	getGeolcation = () => {
-		if ("geolocation" in navigator) {
-			navigator.geolocation.watchPosition((position) => {
-				console.log(position)
-				//send("geo", { user_id: this.state.user_id, coords: position.coords });
-			}, (err) => {
-				this.getGeolcation();
-				this.setPopout(
-					<Alert
-						header="Вы запретили геолокацию"
-						text="Для нормальной работы сервиса разрешите геолокацию"
-						onClose={() => this.setPopout(null)}
-					>
-
-					</Alert>)
-			});
-		} else {
-			this.setPopout(
-				<Alert
-					header="выоваырлоафылалрфыв"
-					text="Для нормальной работы сервиса разрешите геолокацию"
-					onClose={() => this.setPopout(null)}
-				>
-
-				</Alert>)
-		}
+				navigator.geolocation.watchPosition((pos) => {
+					this.state.socket.emit("geo", { user_id: this.state.user_id, coords:pos.coords})
+						console.log(pos)
+						console.log(insideBoundingBox({ latitude: pos.coords.latitude, longitude: pos.coords.longitude }, getBoundingBox(locations1, 1)) ? "1" : "");
+						console.log(insideBoundingBox({ latitude: pos.coords.latitude, longitude: pos.coords.longitude }, getBoundingBox(locations2, 1)) ? "2" : "");
+					//send("coor-users", { user_id: this.state.user_id, lat: position.coords.latitude, lon: position.coords.longitude, number: 1 });
+				}), ((err) => {
+					this.getGeolcation();
+					this.setPopout(
+						<Alert
+							header="Вы запретили геолокацию"
+							text="Для нормальной работы сервиса разрешите геолокацию"
+							onClose={() => this.setPopout(null)}
+						>
+	
+						</Alert>)
+				})
 	}
 
 	setPopout = (popout) => {
@@ -83,7 +84,7 @@ class App extends Component {
 	createQR = () => {
 		this.setPopout(<ScreenSpinner />)
 		send("user", this.state.user).then(data => {
-			const qrSvg = vkQr.createQR("https://user267319094-r7wx5wi4.wormhole.vk-apps.com/#user" + data.user_id, {
+			const qrSvg = vkQr.createQR("https://best11-mice.surge.sh/#user" + data.user_id, {
 				qrSize: 256,
 				isShowLogo: false,
 				className: "QR-container__qr-code"
@@ -128,30 +129,46 @@ class App extends Component {
 
 												<Div>
 													<Button disabled={!this.state.current_answer_id} stretched onClick={() => {
-														send("answer-user", { user_id: this.state.user_id, questions_answer_id: this.state.current_answer_id })
 														this.setActiveModal(null)
+														send("answer-user", { user_id: this.state.user_id, questions_answer_id: this.state.current_answer_id })
 														let dataQuestions = this.state.dataQuestions;
 														let i = dataQuestions.findIndex(i => i.id == question.id);
-														dataQuestions.splice(i, 1);
-														//let q = [].slice.call(document.getElementsByClassName("question"))[i];
+														if(this.state.current_answer_id === 13 || this.state.current_answer_id == 14) {
+															this.setActiveModal("share")
+														}
+														
 														//this.setState({ dataQuestions, current_answer_id: null });
-														this.setState({ dataQuestions, current_answer_id: null })
-													}} size="l">Ответить</Button>
+															dataQuestions.splice(i, 1);
+															this.setState({ dataQuestions, current_answer_id: null })
+														//this.setState({ dataQuestions, current_answer_id: null })
+													}} size="l" style={{ backgroundColor: "#97EDAA" }}>Ответить</Button>
 												</Div>
 											
 										</div>
 									}
+								</ModalPage>
+
+								<ModalPage id="share" header={<ModalPageHeader>Ваше мнение</ModalPageHeader>} onClose={() => this.setActiveModal(null)}>
+									<Div>
+										<Textarea onChange={(e) => this.setState({ comment: e.target.value })} />
+									</Div>
+									<Div>
+										<Button size="l" stretched onClick={() => {
+											send("user-last-update", { user_id: this.state.user_id, comment: this.state.comment});
+											this.setActiveModal(null)
+										}} style={{ backgroundColor: "#97EDAA" }}>Отправить</Button>
+									</Div>
 								</ModalPage>
 							</ModalRoot>
 						}>
 							<View activePanel={activePanelAdmin} id="admin">
 								<Panel id="create_user">
 									<PanelHeader>
-										Админ
+										Мы подружимся
 									</PanelHeader>
 									<FormLayout>
 										<FormLayoutGroup>
-											<FormItem top="ФИО">
+											<FormItem top="Как к вам обращаться?">
 												<Input name="name" onChange={this.onChange} value={user.name} type="text" />
 
 											</FormItem>
@@ -183,9 +200,9 @@ class App extends Component {
 											{
 												this.state.dataQuestions && this.state.dataQuestions.map((el, index) => {
 													return (
-														<Card id={el.id} onClick={() => this.showQuestion(el)} className="question">
+														<Card className={el.animation ? "ball duration-10000" : null} id={el.id} onClick={() => this.showQuestion(el)} className="question">
 															<div className="question__content">
-																<span className="question__content-index">{`${index + 1} `}</span>
+																
 																<div className="question__content-text">{el.name}</div>
 															</div>
 														</Card>
