7452c4dc35d7e2354c4acc6427ee5345b98e18b5
Gossteer <GossteerPlus@gmail.com>
2021-03-28 08:28:33 +0300

    seed

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -12,23 +12,22 @@ class QuestionsController extends Controller
     {
         $date_dif_m = date_diff(date_create($user->created_at), date_create())->i;
 
-        if ($date_dif_m > 15) {
-            return response()->json(['question' => Questions::select('id','name')->with('questionsAnswer:id,question_id,answer_id','questionsAnswer.answer:id,name')->find(2)]);
-        }
-        if ($date_dif_m > 30) {
-            return response()->json(['question' => Questions::select('id','name')->with('questionsAnswer:id,question_id,answer_id','questionsAnswer.answer:id,name')->find(3)]);
-        }
-        if ($date_dif_m > 45) {
-            return response()->json(['question' => Questions::select('id','name')->with('questionsAnswer:id,question_id,answer_id','questionsAnswer.answer:id,name')->find(4)]);
-        }
-        if ($date_dif_m >= 59) {
-            return response()->json(['question' => Questions::select('id','name')->with('questionsAnswer:id,question_id,answer_id','questionsAnswer.answer:id,name')->find(5)]);
-        }
-        // if ($date_dif_m > 100) {
-        //     return response()->json(['question' => Questions::select('id','name')->with('questionsAnswer:id,question_id,answer_id','questionsAnswer.answer:id,name')->find(6)]);
+        // if ($date_dif_m > 15) {
+        //     return response()->json(['question' => Questions::select('id','name')->with('questionsAnswer:id,question_id,answer_id','questionsAnswer.answer:id,name')->find(2)]);
+        // }
+        // if ($date_dif_m > 30) {
+        //     return response()->json(['question' => Questions::select('id','name')->with('questionsAnswer:id,question_id,answer_id','questionsAnswer.answer:id,name')->find(3)]);
+        // }
+        // if ($date_dif_m > 45) {
+        //     return response()->json(['question' => Questions::select('id','name')->with('questionsAnswer:id,question_id,answer_id','questionsAnswer.answer:id,name')->find(4)]);
+        // }
+        // if ($date_dif_m >= 59) {
+        //     return response()->json(['question' => Questions::select('id','name')->with('questionsAnswer:id,question_id,answer_id','questionsAnswer.answer:id,name')->find(5)]);
         // }
 
-        return response()->json(['data' => $date_dif_m, $user->created_at, date_create(), date_create($user->created_at)]);
+        // return response()->json(['question' => Questions::select('id','name')->with('questionsAnswer:id,question_id,answer_id','questionsAnswer.answer:id,name')->find(6)]);
+        
+        return response()->json(['question' => Questions::select('id','name')->with('questionsAnswer:id,question_id,answer_id','questionsAnswer.answer:id,name')->get()]);
     }
 
     public function getFirstQuestions()
