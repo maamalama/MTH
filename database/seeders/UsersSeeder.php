83a50521aa42ceff5a112837e3e44776ffa06b3d
Gossteer <GossteerPlus@gmail.com>
2021-03-28 03:18:46 +0300

    seed

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -14,22 +14,22 @@ class UsersSeeder extends Seeder
      */
     public function run()
     {
-        // if (($handle = fopen(public_path() . '/comment/comments.csv', 'r')) !== FALSE) {
-        //     while (($data = fgetcsv($handle)) !== FALSE) {
-        //         $full_date[] = $data[0];
-        //         $full_date2[] = $data[1];
-        //     }
-        //     fclose($handle);
-        // }
+        if (($handle = fopen(public_path() . '/comment/comments.csv', 'r')) !== FALSE) {
+            while (($data = fgetcsv($handle)) !== FALSE) {
+                $full_date[] = $data[0];
+                $full_date2[] = $data[1];
+            }
+            fclose($handle);
+        }
 
-        // $count_date = count($full_date) - 1;
+        $count_date = count($full_date) - 1;
 
-        // for ($i = 0; $i < 1000; $i++) {
-        //     User::factory(1)->create([
-        //         'comment' => $full_date[rand(0, $count_date)],
-        //         'comment_positively' => (int) $full_date2[rand(0, $count_date)]
-        //     ]);
-        // }
+        for ($i = 0; $i < 1000; $i++) {
+            User::factory(1)->create([
+                'comment' => $full_date[rand(0, $count_date)],
+                'comment_positively' => (int) $full_date2[rand(0, $count_date)]
+            ]);
+        }
 
         for ($i = 0; $i < 1000; $i++) {
             switch ((rand(0, 4))) {
